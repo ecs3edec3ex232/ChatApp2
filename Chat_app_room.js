e12733f938074de4ba6b64002d9fa74af5efe488
Emma Remon <90062286+ecs3edec3ex232@users.noreply.github.com>
2021-11-21 20:27:38 -0500

    Add files via upload

---
+++ Chat_app_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var  firebaseConfig = {
+    apiKey: "AIzaSyBuykrU6zuK_PAAqdK0suPL5pSApoi-6JU",
+    authDomain: "chat-app-5dc7b.firebaseapp.com",
+    projectId: "chat-app-5dc7b",
+    storageBucket: "chat-app-5dc7b.appspot.com",
+    messagingSenderId: "173554177331",
+    appId: "1:173554177331:web:6ed79dd27975eb60f15a46"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
